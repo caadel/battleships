81bcfbaae10b7ca268fb279b43144da6fd846239
Adam Elfström <46933430+caadel@users.noreply.github.com>
2022-02-11 19:34:34 +0100

    Add interactive GUI

--- src/gui.ts
+++ src/gui.ts
@@ -1,33 +1,161 @@
-const gameArea = document.body.appendChild(createElement('main', 'game-area'))
-const player1Board = gameArea.appendChild(
-  createElement('div', 'board', 'player-1')
+/**
+ * This module will only ever interact directly with the HTML.
+ * It is responsible for updating UI elements based on input given
+ * from the caller (i.e. the controller).
+ *
+ * gui.ts, style.css, and index.html essentially makes up the "View" in MVC.
+ */
+
+/**
+ * These elements can be read by the controller after calling init(),
+ * so the controller can listen to view events.
+ */
+const interactables = {
+  player1Board: HTMLElement.prototype,
+  player1SetupBoard: HTMLElement.prototype,
+  player2Board: HTMLElement.prototype,
+  player2SetupBoard: HTMLElement.prototype,
+}
+const currentPlayerDisplay = document.body.appendChild(
+  createElement('h1', 'info-display')
 )
-const player2Board = gameArea.appendChild(
-  createElement('div', 'board', 'player-2')
+const infoDisplay = document.body.appendChild(
+  createElement('h3', 'info-display')
 )
 
+/**
+ * Initialises GUI components.
+ * @returns Game boards.
+ */
 function init() {
-  createBoard(player1Board)
-  createBoard(player2Board)
-  player1Board.classList.add('disabled')
+  // Create game and setup boards
+  const gameArea = document.body.appendChild(createElement('main', 'game-area'))
+  interactables.player1Board = gameArea.appendChild(
+    createElement('div', 'board')
+  )
+  interactables.player2Board = gameArea.appendChild(
+    createElement('div', 'board')
+  )
+
+  const setupArea = gameArea.appendChild(createElement('div', 'setup-area'))
+  interactables.player1SetupBoard = setupArea.appendChild(
+    createElement('div', 'setup-board')
+  )
+  interactables.player2SetupBoard = setupArea.appendChild(
+    createElement('div', 'setup-board')
+  )
+
+  // Populate the boards
+  populateBoard(interactables.player1SetupBoard)
+  populateBoard(interactables.player1Board)
+  populateBoard(interactables.player2Board)
+  populateBoard(interactables.player2SetupBoard)
+
+  // Add some written instructions
+  const instructionTitle = createElement('h2', 'info-display')
+  instructionTitle.innerText = 'Instructions'
+  const instructions1 = createElement('div', 'info-display', 'instructions')
+  instructions1.innerHTML = `<strong>When placing:</strong> click to place ship, right-click to rotate ship`
+  const instructions2 = createElement('div', 'info-display', 'instructions')
+  instructions2.innerHTML = `<strong>During firing:</strong> click to fire!`
+  document.body.append(instructionTitle, instructions1, instructions2)
+
+  return {
+    interactables,
+  }
 }
 
 /**
- * Adds grid cells to a an HTML element and sets up click events.
- * @param board The element to add the cells to.
+ * Updates the general information display area with the given text.
+ * @param text Text content to show.
  */
-function createBoard(board: Element) {
-  for (let i = 0; i < 100; i++) {
-    board.appendChild(createElement('div'))
+function updateInfoDisplay(text: string) {
+  infoDisplay.innerText = text
+}
+
+/**
+ * Updates the current player text display.
+ * @param id Player ID.
+ */
+function updatePlayerDisplay(id: number) {
+  currentPlayerDisplay.innerText = `Player ${id}'s turn`
+}
+
+function markCellAsShip(cell: Element) {
+  cell.classList.add('placement')
+}
+
+function markCellAsMiss(cell: Element) {
+  cell.classList.add('hit')
+}
+
+function markCellAsHit(cell: Element) {
+  cell.classList.add('hit')
+}
+
+function clearCell(cell: Element) {
+  cell.classList.remove('placement')
+}
+
+function placeShipOnBoard(board: Element, x: number, y: number) {
+  board.children[x * y].classList.add('placement')
+}
+
+/**
+ * Shows the game or setup board of the given player.
+ * Deactivates the board not in use.
+ * @param isSetupPhase True if the setup-boards are to be shown.
+ * @param isFirstPlayer True if the first player is to play.
+ */
+function showPlayerBoard(isSetupPhase: boolean, isFirstPlayer: boolean) {
+  // setup = false, first = false
+  let boardToHide = interactables.player2Board
+  let boardToShow = interactables.player1Board
+
+  if (isFirstPlayer) {
+    if (!isSetupPhase) {
+      // setup = false, first = true
+      boardToHide = interactables.player1Board
+      boardToShow = interactables.player2Board
+    } else {
+      // setup = true, first = true
+      boardToHide = interactables.player2SetupBoard
+      boardToShow = interactables.player1SetupBoard
+    }
+  } else if (isSetupPhase) {
+    // setup = true, first = false
+    boardToHide = interactables.player1SetupBoard
+    boardToShow = interactables.player2SetupBoard
+
+    for (const cell of interactables.player1SetupBoard.children) {
+      clearCell(cell)
+    }
   }
-  board.addEventListener('click', (e) => {
-    const target = e.target
 
-    if (target === board) return
+  boardToHide.classList.add('disabled')
+  boardToShow.classList.remove('disabled')
+}
 
-    // TODO: Handle click on non-hit/missed tile here
-    console.log(target)
-  })
+/**
+ * Removes the setup boards from the GUI by hiding them.
+ */
+function hideSetupArea() {
+  interactables.player1SetupBoard.parentElement?.classList.add('hidden')
+}
+
+/**
+ * Adds grid cells to a an HTML container.
+ * @param board The container element to add the cells to.
+ */
+function populateBoard(board: Element) {
+  for (let y = 0; y < 10; y++) {
+    for (let x = 0; x < 10; x++) {
+      const element = createElement('div')
+      element.dataset.x = x.toString()
+      element.dataset.y = y.toString()
+      board.appendChild(element)
+    }
+  }
 }
 
 /**
@@ -44,4 +172,13 @@ function createElement(tagname: string, ...classes: string[]) {
 
 export default {
   init,
+  showPlayerBoard,
+  updateInfoDisplay,
+  updatePlayerDisplay,
+  markCellAsShip,
+  markCellAsHit,
+  markCellAsMiss,
+  clearCell,
+  placeShipOnBoard,
+  hideSetupArea,
 }
